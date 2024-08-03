7b722cf97702d4a071792f28b103e4d31ca8ba8c
Maxime Mllt <mlltmax66@gmail.com>
2024-08-03 16:37:16 +0200

    build

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,13 +12,12 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Righteous&display=swap"
         rel="stylesheet">
-    <!-- Font Awesome Icons -->
-    <!-- Scripts -->
+
     <link rel="stylesheet" href="https://unpkg.com/@themesberg/flowbite@1.2.0/dist/flowbite.min.css"/>
 {{--    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>--}}
-    <link rel="stylesheet" href="{{ asset('css/fontawesome.min.css') }}">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('css/fontawesome.css') }}">
 </head>
 <body class="font-sans antialiased">
 <div class="min-h-screen bg-gray-100">
