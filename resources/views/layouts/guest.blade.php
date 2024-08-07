15b9625f6adcbd33872ba2a664a8464b98a08372
Maxime Mllt <mlltmax66@gmail.com>
2024-08-07 09:53:30 +0200

    head + build

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,13 +5,16 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Celul</title>
+        <link rel="shortcut icon" href="{{ asset("images/icon.png") }}">
+        <link rel="icon" type="image/png" href="{{ asset("images/icon.png") }}">
+        <link rel="apple-touch-icon" sizes="76x76" href="{{ asset("images/icon.png") }}">
 
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Righteous&display=swap" rel="stylesheet">
         <!-- Font Awesome Icons -->
-        <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="{{ asset('css/fontawesome.css') }}">
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
