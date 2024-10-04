894f4cd0724018ab2da731796394b5dd13330e94
Maxime Mllt <mlltmax66@gmail.com>
2024-10-04 11:02:30 +0200

    mail styles

--- resources/views/emails/login.blade.php
+++ resources/views/emails/login.blade.php
@@ -26,7 +26,7 @@
                 Votre lien de connexion
             </h1>
             <div style="padding:27px 0 27px">
-                <a href="{{ route('loginWithToken', 'fvrced') }}"
+                <a href="{{ route('loginWithToken', $token) }}"
                    style="letter-spacing: 1px;text-transform: uppercase; text-decoration:none;display:block;max-width:100%;background-color:#0075F0;border-radius:4px;font-weight:500;color:#fff;font-size:15px;text-align:center;padding:14px 32px 14px 32px"
                    target="_blank">
                     <span
@@ -37,14 +37,14 @@
             </div>
 
             <p style="padding-bottom: 12px">
-                Lien de connexion : <a href="{{ route('loginWithToken', 'fvrced') }}"
-                                       target="_blank">{{ route('loginWithToken', 'fvrced') }}</a>
+                Lien de connexion : <a href="{{ route('loginWithToken', $token) }}"
+                                       target="_blank">{{ route('loginWithToken', $token) }}</a>
             </p>
             <p style="font-size:15px;line-height:1.4;margin:0 0 15px;color:#3c4149">
                 Important : ce lien expire à {{ now()->addHours(2)->addMinutes(10)->format('d/m/Y à h:i') }}
             </p>
             <hr style="width:100%;border:none;border-top:1px solid #eaeaea;border-color:#dfe1e4;margin:42px 0 26px"/>
-            <a href="https://google.com" style="color:#b4becc;text-decoration:none;font-size:14px" target="_blank">Pbi
+            <a href="https://dev.pbitoolbox.com/" style="color:#b4becc;text-decoration:none;font-size:14px" target="_blank">Pbi
                 toolbox</a>
         </div>
     </div>
