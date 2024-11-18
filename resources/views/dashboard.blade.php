7b6c0a06db860f4afbd66984c6da8418cecf6287
Maxime Mllt <mlltmax66@gmail.com>
2024-11-18 15:05:02 +0100

    text

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex justify-between items-center">
             <h2 class="font-medium text-xl text-gray-800 leading-tight">
-                Tableau de board
+                Tableau de bord
             </h2>
         </div>
     </x-slot>
